dca7785c1a20b8f2fff076153aa90e8e1c4f6d12
Alexander Kislitsyn <4lex.kislitsyn@gmail.com>
2020-04-21 08:07:31 +0300

    Initial commit of bot

---
+++ TinkoffRateBot/BotConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinkoffRateBot
+{
+    public class BotConfiguration
+    {
+        public string Name { get; set; }
+        public string Key { get; set; }
+        public Uri WebHookUrl { get; set; }
+    }
+}
