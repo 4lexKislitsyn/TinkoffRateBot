f6b4c9580fcd9ddd11890181a1dc1dc55da1fc67
Alexander Kislitsyn <4lex.kislitsyn@gmail.com>
2020-04-21 14:40:22 +0300

    Added DAL models.

---
+++ TinkoffRateBot/DataAccess/Models/TelegramChatInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinkoffRateBot.DataAccess.Models
+{
+    public class TelegramChatInfo
+    {
+        public int Id { get; set; }
+        public bool IsEnabled { get; set; }
+        public DateTime Added { get; set; }
+    }
+}
---
+++ TinkoffRateBot/DataAccess/Models/TinkoffExchangeRate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinkoffRateBot.DataAccess.Models
+{
+    public class TinkoffExchangeRate
+    {
+        public string From { get; set; }
+        public string Category { get; set; }
+        public double Sell { get; set; }
+        public double Buy { get; set; }
+    }
+}
